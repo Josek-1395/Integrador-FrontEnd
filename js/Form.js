971feea5db72c4d8c63091ae8b7e0a5b4b964be4
Jose <jose.khairallah.utn@gmail.com>
2022-11-06 02:40:11 -0300

    agregado JS

---
+++ js/Form.js
@@ -0,0 +1,18 @@
+function resumen(){
+    const valor_ticket_inicial = 200;
+    let cantidad = document.getElementById("cantidad").value;
+    let categoria = descuento(document.getElementById("categoria").value);
+    let valor_tickets = valor_ticket_inicial * cantidad * categoria;
+    document.getElementById("valor-final-tickets").placeholder = "Total a pagar: $" + valor_tickets;
+}
+function descuento(x){
+    const descuento_estudiante = 0.8;
+    const descuento_trainee = 0.5;
+    const descuento_junior = 0.15;
+    if(x==1){return(descuento_estudiante);}
+    if(x==2){return(descuento_trainee);}
+    if(x==3){return(descuento_junior);}
+}
+function borrar(){
+    document.getElementById("valor-final-tickets").placeholder = "Total a pagar: $";
+}
