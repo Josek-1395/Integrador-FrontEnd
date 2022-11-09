af4d7204e24b64380b277fe2436798cb130706a6
Jose <jose.khairallah.utn@gmail.com>
2022-11-08 23:04:47 -0300

    discount fix

--- js/Form.js
+++ js/Form.js
@@ -2,8 +2,8 @@ function resumen(){
     const valor_ticket_inicial = 200;
     let cantidad = document.getElementById("cantidad").value;
     let categoria = descuento(document.getElementById("categoria").value);
-    let valor_tickets = valor_ticket_inicial * cantidad * categoria;
-    document.getElementById("valor-final-tickets").placeholder = "Total a pagar: $" + valor_tickets;
+    let valor_tickets = valor_ticket_inicial * cantidad * ( 1 - categoria);
+    document.getElementById("valor-final-tickets").placeholder = "Total a pagar: $" + valor_tickets.toFixed(2);
 }
 function descuento(x){
     const descuento_estudiante = 0.8;
